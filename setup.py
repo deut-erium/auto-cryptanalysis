fde6065eeeed2ffd6521b1fcaf67202ab3843752
deut-erium <himanshuthesheoran@gmail.com>
2023-08-28 03:21:05 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name="cryptanalysis",
-    version="0.0.1",
+    version="0.0.3",
     packages=find_packages(),
     author="Himanshu Sheoran",
     description="Automated cryptanalysis library for substitution permutation network",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/deut-erium/auto-cryptanalysis",
-    license='MIT',
+    keywords="cryptanalysis differential linear cryptography SPN cipher crypto",
+    license="GPL",
     install_requires=[
         "z3-solver",
         "tqdm"
     ],
     classifiers=[
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
@@ -25,8 +26,12 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
+    project_urls={
+        "Documentation": "https://deut-erium.github.io/auto-cryptanalysis",
+        "Source Code": "https://github.com/deut-erium/auto-cryptanalysis"
+    },
     include_package_data=True,
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
 
 
